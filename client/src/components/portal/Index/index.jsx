82703e7c3e17c2ead1a2a9db94021ab323abb733
fs1762920 <fs1762920@163.com>
2024-08-16 23:25:34 +0800

    样式调整

--- client/src/components/portal/Index/index.jsx
+++ client/src/components/portal/Index/index.jsx
@@ -6,44 +6,6 @@ import { dateFormat } from '@/utils/dateUtils'
 import { $get } from "@/api/RestUtils";
 import "./index.less";
 
-const articleList = [
-  {
-    id: 0,
-    title: "vue整合antd",
-    introduction:
-      "This post is originated from here and is used for testing markdown style. This post contains nearly every markdown usage. Make sure all the markdown elements below show up correctly.",
-    createTime: "2024-01-23",
-  },
-  {
-    id: 1,
-    title: "react整合antd",
-    introduction:
-      "This post is originated from here and is used for testing markdown style. This post contains nearly every markdown usage. Make sure all the markdown elements below show up correctly.",
-    createTime: "2024-01-23",
-  },
-  {
-    id: 2,
-    title: "Code Highlight Style test",
-    introduction:
-      "Make sure all the code blocks highlighted correctly. All the code samples are come from the demo of https://highlightjs.org",
-    createTime: "2024-01-23",
-  },
-  {
-    id: 3,
-    title: "Code Highlight Style test",
-    introduction:
-      "Make sure all the code blocks highlighted correctly. All the code samples are come from the demo of https://highlightjs.org",
-    createTime: "2024-01-23",
-  },
-  {
-    id: 4,
-    title: "Code Highlight Style test",
-    introduction:
-      "Make sure all the code blocks highlighted correctly. All the code samples are come from the demo of https://highlightjs.org",
-    createTime: "2024-01-23",
-  },
-];
-
 function index() {
   
   const navigate = useNavigate();
@@ -93,7 +55,10 @@ function index() {
         <div className="article-item" key={item.articleId} onClick={() => toDetail(item.articleId)}>
           <div className="title">{item.title}</div>
           <div className="introduction">{item.introduction}</div>
-          <div className="date">{dateFormat(item.ctime)}</div>
+          <div className="foot">
+            <span className="category">{"#" + item.categoryName}</span>
+            <span className="date">{dateFormat(item.ctime)}</span>
+          </div>
         </div>
       );
     });
